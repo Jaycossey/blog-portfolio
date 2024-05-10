e9bd302a18bc9bd11cc7b1a98ec4ca35ea35deda
Ian Scott <jaycossey@live.com>
2024-05-10 22:01:27 +0100

    added portfolioController, portfolio model, portfolio index view, a dbset to the dbcontext, updated readme with steps, need to run migrations and update database, need to create new seeddata

---
+++ BlogPortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,25 @@
+﻿using BlogPortfolio.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using System.Text.Encodings.Web;
+
+namespace BlogPortfolio;
+
+public class PortfolioController : Controller
+{
+    private readonly BlogPortfolioDbContext _context;
+
+    public PortfolioController(BlogPortfolioDbContext context)
+    {
+        _context = context;
+    }
+
+    // 
+    // GET: /Portfolio/
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- BlogPortfolio/Models/SeedData.cs
+++ BlogPortfolio/Models/SeedData.cs
@@ -56,6 +56,7 @@ public static class SeedData
                     }
                 );
                 context.SaveChanges();
+
             }
     }
 }
